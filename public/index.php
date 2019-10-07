7e9739d39ff1e277da648aec79a3b7952cbf4f2f
MatsuP8 <matsumoto.s.s.0805@gmail.com>
2019-10-07 20:21:18 +0900

    routingをrouteファイルへ移譲

--- public/index.php
+++ public/index.php
@@ -1,26 +1,14 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$routes = [];
+$routes = require __DIR__ . '/../app/routes.php';
 
-$routes['/'] = function() {
-    echo "<!DOCTYPE html>\n";
-    echo "<title>test</title>\n";
-    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-    echo "<ul><li><a href='phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-    exit;
-};
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$routes['/phpinfo.php'] = function(){
-    phpinfo();
-};
-
-if(isset($routes[$_SERVER['REQUEST_URI']])){
-    $f = $routes[$_SERVER['REQUEST_URI']];
+if (isset($routes[$request_uri])) {
+    $f = $routes[$request_uri];
     $f();
-    exit;
+} else {
+    http_response_code(404);
+    echo "<h1>404 Not Found</h1>";
 }
-
-http_response_code(404);
-
-echo "<h1>404 Not Found</h1>";
