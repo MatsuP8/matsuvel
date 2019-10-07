51f2c218dc8459e274225061d3bc1c65accf2884
MatsuP8 <matsumoto.s.s.0805@gmail.com>
2019-10-07 20:12:45 +0900

    routingの設定

--- public/index.php
+++ public/index.php
@@ -1,7 +1,26 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "<!DOCTYPE html>\n";
-echo "<title>test</title>\n";
-echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+$routes = [];
+
+$routes['/'] = function() {
+    echo "<!DOCTYPE html>\n";
+    echo "<title>test</title>\n";
+    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
+    echo "<ul><li><a href='phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+    exit;
+};
+
+$routes['/phpinfo.php'] = function(){
+    phpinfo();
+};
+
+if(isset($routes[$_SERVER['REQUEST_URI']])){
+    $f = $routes[$_SERVER['REQUEST_URI']];
+    $f();
+    exit;
+}
+
+http_response_code(404);
+
+echo "<h1>404 Not Found</h1>";
--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+
 function h(string $str)
 {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
