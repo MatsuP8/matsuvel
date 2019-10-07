3feb0b1b810fb6c18a37375810c9828258c9d2de
MatsuP8 <matsumoto.s.s.0805@gmail.com>
2019-10-07 20:47:53 +0900

    routeファイルでstatus_code,$header,$bodyを返すようにした。

--- app/routes.php
+++ app/routes.php
@@ -1,19 +1,17 @@
 <?php
 $routes = [];
 
-$routes['/'] = function() {
-    echo "<!DOCTYPE html>\n";
-    echo "<title>test</title>\n";
-    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
-    echo "<ul><li><a href='phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-    exit;
+$routes['/'] = function () {
+    ob_start();
+    include __DIR__ . '/../app/view/index.phtml';
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
-$routes['/phpinfo.php'] = function(){
+$routes['/phpinfo.php'] = function () {
+    ob_start();
     phpinfo();
-    exit;
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
-
 
 return $routes;
 
